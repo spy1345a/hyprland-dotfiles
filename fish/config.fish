aed6800defa799472402d16209ccf462761d4097
spy <ajeshmb24@gmail.com>
2026-03-02 17:49:22 +0530

    Auto backup: 2026-03-02 17:49:22

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+
+# Smart history search
+bind \e\[A history-search-backward
+bind \e\[B history-search-forward
+
+end
